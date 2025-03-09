eb9c88d17ff7e8091ad9c2167572791b55fbcf21
jenizeth-chess <jenizethlucii@gmail.com>
2025-03-09 13:38:45 -0600

    02 ocultar powerd by

--- myApp.js
+++ myApp.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const app = express();
-
+const helmet = require("helmet");
 // your code
 
 module.exports = app;
 const api = require('./server.js');
+
 app.use(express.static('public'));
+
+//2. Desabilitar powered by
+app.use(helmet.hidePoweredBy());
+
 app.disable('strict-transport-security');
+
 app.use('/_api', api);
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
 let port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`♟️ Lucia info security app started on port ${port}`);
 });
