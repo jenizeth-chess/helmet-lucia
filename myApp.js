660539a831ba3144d0170715e79ec30c6228b883
jenizeth-chess <jenizethlucii@gmail.com>
2025-03-26 19:00:27 -0600

    10 CSP 5

--- myApp.js
+++ myApp.js
@@ -41,8 +41,8 @@ app.use(helmet.noCache());
 app.use(
 	helmet.contentSecurityPolicy({
 	directives: {
-		"default-src":["'self'"],
-		"script-src":["'self'","'trusted-cdn.com'"]
+		defaultSrc:["'self'"],
+		scriptSrc:["'self'",'trusted-cdn.com']
 	},
 	})
 );
